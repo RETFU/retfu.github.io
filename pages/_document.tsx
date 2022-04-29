1469399d43f163528a524c203c069c5176710555
Fabien Furet <fabien@iwd.io>
2022-04-29 20:43:57 +0200

    fix: better font handling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -47,7 +47,7 @@ export default class MyDocument extends Document {
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤓</text></svg>"
           />
           <link
-            href="https://fonts.googleapis.com/css?family=Work+Sans:400,500&display=optional"
+            href="https://fonts.googleapis.com/css?family=Work+Sans:400,500&display=swap"
             rel="stylesheet"
           />
         </Head>
